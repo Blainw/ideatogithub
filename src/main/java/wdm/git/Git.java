887dc829fd371d83f3dca3dcd2cef1771f95381f
wdm <1831486177@qq.com>
2022-06-25 16:26:13 +0800

    third commit by idea

--- src/main/java/wdm/git/Git.java
+++ src/main/java/wdm/git/Git.java
@@ -3,5 +3,6 @@ package wdm.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("git测试，看是不是可以提示此版本变更了");
+        System.out.println("再来测试一下变换");
     }
 }
