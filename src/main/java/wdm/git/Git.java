ebaaa0fb9e8c096d4573787096b102b20e5f740f
wdm <1831486177@qq.com>
2022-06-25 16:46:16 +0800

    hot-fix commit1

--- src/main/java/wdm/git/Git.java
+++ src/main/java/wdm/git/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("git测试，看是不是可以提示此版本变更了");
         System.out.println("再来测试一下变换");
+        System.out.println("hot-fix test");
     }
 }
